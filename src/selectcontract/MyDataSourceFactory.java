d750eee0087833515d5d7708dee525e4a111d142
tuckw <tuckw@10.0.0.138>
2020-03-31 13:17:36 -0700

    Finished step 4. onto step 5!

---
+++ src/selectcontract/MyDataSourceFactory.java
@@ -0,0 +1,29 @@
+package selectcontract;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.sql.DataSource;
+import oracle.jdbc.pool.OracleDataSource;
+
+public class MyDataSourceFactory {
+    
+    public static DataSource getOracleDataSource() throws SQLException {
+        
+        Properties props = new Properties();
+        FileInputStream fis = null;
+        OracleDataSource oracleDS = null;
+        try {
+            fis = new FileInputStream("C:\\Users\\tuckw\\OneDrive\\ICS_WINTER_2020\\ICS125\\SelectContractLab08\\Lab08\\src\\selectcontract\\db.properties");
+            props.load(fis);
+            oracleDS = new OracleDataSource();
+            oracleDS.setURL(props.getProperty("ORACLE_DB_URL"));
+            oracleDS.setUser(props.getProperty("ORACLE_DB_USERNAME"));
+            oracleDS.setPassword(props.getProperty("ORACLE_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return oracleDS;
+    } // end getOracleDataSource
+} // end class
